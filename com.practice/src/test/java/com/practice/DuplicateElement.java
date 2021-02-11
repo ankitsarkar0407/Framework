3b4678f22177d98f01ad6df6b057973adb5c0e44
Ankit Sarkar <ankesrkr@gmail.com>
2021-02-11 15:33:02 +0530

    Duplicate Element code

--- com.practice/src/test/java/com/practice/DuplicateElement.java
+++ com.practice/src/test/java/com/practice/DuplicateElement.java
@@ -1,6 +1,9 @@
 package com.practice;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class DuplicateElement {
@@ -9,8 +12,25 @@ public class DuplicateElement {
 		
 		
 		int arr[] = { 1, 2, 8, 2, 9,6, 5 ,8 , 4 } ;
+		List<Integer> list = new ArrayList<Integer>();
 		
-	 //	Set<Integer> uniqueValue = new Hashset();
+	 	Set<Integer> uniqueValue = new HashSet<Integer>();
+	 	boolean isDuplicate = false;
+	 	
+	 	for(int i = 0 ; i < arr.length;i++) {
+	 		if(uniqueValue.contains(arr[i])) {
+	 			 isDuplicate = true;	
+	 			 list.add(arr[i]);
+	 			
+	 		}
+	 		else {
+	 			uniqueValue.add(arr[i]);
+	 		}
+	 		
+	 	}
+	 	System.out.println(isDuplicate);
+	 	System.out.println(uniqueValue);
+	 	System.out.println("Duplicate elements = " +list);
 		
 		
 	}
@@ -19,6 +39,7 @@ public class DuplicateElement {
 	public static void main (String args[]) {
 		
 		DuplicateElement obj = new DuplicateElement();
+		obj.solution();
 		
 	}
 }
